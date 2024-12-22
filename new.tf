6e77cb1f3c9f0dab9293f7944986ff63d330821c
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-12-22 13:55:25 +0400

    terraform code for eks cluster

---
+++ new.tf
@@ -0,0 +1,56 @@
+
+terraform {
+  required_providers { // Cloud API provider 
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+
+// Create an EKS cluster
+resource "aws_eks_cluster" "aws_eks" {
+  name     = "eks_cluster_demo"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  // Configure VPC for the EKS cluster
+  vpc_config {
+    subnet_ids = var.subnet_ids // provide the subnet ids
+  }
+
+  // Add tags to the EKS cluster for identification
+  tags = {
+    Name = "EKS_demo"
+  }
+}
+
+
+
+// Create an EKS node group
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.aws_eks.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = var.subnet_ids
+
+  // Configure scaling options for the node group
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  // Ensure that the creation of the node group depends on the IAM role policies being attached
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,29 @@
+
+
+variable "desired_size" {
+  description = "The desired number of worker nodes"
+  type        = number
+  default     = 1
+}
+variable "max_size" {
+  description = "The maximum number of worker nodes"
+  type        = number
+  default     = 1
+}
+variable "min_size" {
+  description = "The minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "node_group_name" {
+  description = "The name of the node group"
+  type        = string
+  default     = "node_demo"
+}
+
+variable "subnet_ids" {
+  description = "The subnet IDs for the EKS node group"
+  type        = list(string)
+  default     = ["subnet-07397980c132d51e6", "subnet-0c38690de6cd35a27"]
+}
