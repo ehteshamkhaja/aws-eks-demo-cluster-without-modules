fd8e29fcb64f72548596e0b56584bc3640aeb9e6
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2025-01-07 17:28:28 +0400

    sample eks user permission and access code

--- test-user.tf
+++ test-user.tf
@@ -1,19 +1,30 @@
-/*
-data "aws_iam_user" "example" {
+
+data "aws_iam_user" "devops" {
   user_name = "devops"
 }
 
+data "aws_iam_user" "eksdeveloper" {
+  user_name = "eksdeveloper"
+}
+
 resource "aws_eks_access_entry" "example" {
-  cluster_name  = var.cluster_name
-  principal_arn = data.aws_iam_user.example.arn
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  principal_arn = data.aws_iam_user.devops.arn
+  # kubernetes_groups = ["group-1", "group-2"]
+  type = "STANDARD"
+}
+
+resource "aws_eks_access_entry" "eksdeveloper" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  principal_arn = data.aws_iam_user.eksdeveloper.arn
   # kubernetes_groups = ["group-1", "group-2"]
   type = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "eks-cluster-admin-policy-1" {
-  cluster_name  = var.cluster_name
+  cluster_name  = aws_eks_cluster.eks-cluster.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-  principal_arn = data.aws_iam_user.example.arn
+  principal_arn = data.aws_iam_user.devops.arn
 
   access_scope {
     type = "cluster"
@@ -21,11 +32,12 @@ resource "aws_eks_access_policy_association" "eks-cluster-admin-policy-1" {
   }
   depends_on = [aws_eks_access_entry.example]
 }
+
 
 resource "aws_eks_access_policy_association" "eks-cluster-admin-policy-2" {
-  cluster_name  = var.cluster_name
+  cluster_name  = aws_eks_cluster.eks-cluster.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = data.aws_iam_user.example.arn
+  principal_arn = data.aws_iam_user.devops.arn
 
   access_scope {
     type = "cluster"
@@ -34,7 +46,23 @@ resource "aws_eks_access_policy_association" "eks-cluster-admin-policy-2" {
   depends_on = [aws_eks_access_entry.example]
 }
 
-output "user_arn" {
-  value = data.aws_iam_user.example.arn
+resource "aws_eks_access_policy_association" "eks-admin-policy-3" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  principal_arn = data.aws_iam_user.eksdeveloper.arn
+
+  access_scope {
+    type = "namespace"
+     namespaces = ["default"]
+  }
+  depends_on = [aws_eks_access_entry.example]
 }
-*/
+
+
+output "user_arn" {
+  value = data.aws_iam_user.devops.arn
+}
+
+output "user_arn_eksdeveloper" {
+  value = data.aws_iam_user.eksdeveloper.arn
+}
