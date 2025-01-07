b99af80323c3be9792d0436f65ffa285a03bb904
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2025-01-07 19:45:35 +0400

    Create eks-addons.tf

---
+++ eks-addons.tf
@@ -0,0 +1,6 @@
+resource "aws_eks_addon" "eks-addon" {
+
+  for_each     = toset(["coredns", "vpc-cni", "kube-proxy", "kube-dns", "aws-node", "eks-pod-identity-agent"])
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name   = each.key
+}
