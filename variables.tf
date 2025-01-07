643d10f30266eed4eb118823dbb72a296fb36e1c
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2025-01-07 14:49:58 +0400

    variable creation for instance type in nodegroup

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "region" {}
 variable "cluster_name" {}
+variable "instance_types" {}
 variable "public-subnet-1-cidr"{}
 variable "public-subnet-2-cidr"{}
 variable "private-subnet-1-cidr"{}
