84f14be5f1b86ed51e4b361d017cc68700ebd2aa
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2025-01-21 21:53:08 +0400

    Update external-secret-policy.tf

--- external-secret-policy.tf
+++ external-secret-policy.tf
@@ -6,24 +6,24 @@ resource "aws_iam_policy" "external_secret_policy" {
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowAccessToSecretsManager",
-            "Effect": "Allow",
-            "Action": [
-                "secretsmanager:ListSecrets",
-                "secretsmanager:GetSecretValue",
-                "secretsmanager:DescribeSecret",
-                "secretsmanager:ListSecretVersionIds"
-            ],
-            "Resource": "arn:aws:secretsmanager:${var.region}:${var.accountid}:secret:*"
-        }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowAccessToSecretsManager",
+        "Effect" : "Allow",
+        "Action" : [
+          "secretsmanager:ListSecrets",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecretVersionIds"
+        ],
+        "Resource" : "arn:aws:secretsmanager:${var.region}:${var.accountid}:secret:*"
+      }
     ]
-})
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
+resource "aws_iam_role_policy_attachment" "external_secret_iam_role_policy_attach" {
   policy_arn = aws_iam_policy.external_secret_policy.arn
   role       = aws_iam_role.ng-role.name
 }
